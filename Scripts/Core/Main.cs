fee6226664775fb1b32c85efac7ea7e9b01fa579
hecomi <info@hecomi.com>
2015-11-07 11:13:07 +0900

    squash for upm release 0.6.0

---
+++ Scripts/Core/Main.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace uREPL
+{
+
+[RequireComponent(typeof(Window))]
+public class Main : MonoBehaviour
+{
+	public Parameters parameters;
+	public EditorParameters editor;
+	public string preLoadCode = "/* Write code here which you want to run just after start. */";
+
+	void Awake()
+	{
+		Evaluator.Initialize();
+		RunPreLoadCode();
+		GetComponent<Window>().main = this;
+		UpdateAllDefaultCompletionPlugins();
+	}
+
+	void Update()
+	{
+		UpdateAllDefaultCompletionPlugins();
+	}
+
+	void RunPreLoadCode()
+	{
+		Evaluator.Evaluate(preLoadCode, false);
+	}
+
+	void UpdateAllDefaultCompletionPlugins()
+	{
+		UpdateCompletionPlugin<MonoCompletion>(parameters.useMonoCompletion);
+		UpdateCompletionPlugin<CommandCompletion>(parameters.useCommandCompletion);
+		UpdateCompletionPlugin<RuntimeCommandCompletion>(parameters.useRuntimeCommandCompletion);
+		UpdateCompletionPlugin<GameObjectNameCompletion>(parameters.useGameObjectNameCompletion);
+		UpdateCompletionPlugin<GameObjectPathCompletion>(parameters.useGameObjectPathCompletion);
+		UpdateCompletionPlugin<GlobalClassCompletion>(parameters.useGlobalClassCompletion);
+	}
+
+	private void UpdateCompletionPlugin<T>(bool enabled) where T : MonoBehaviour
+	{
+		(GetComponent<T>() ?? gameObject.AddComponent<T>()).enabled = enabled; 
+	}
+}
+
+}
---
+++ Scripts/Core/Mono.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Text;
+
+namespace uREPL
+{
+
+public class MonoMessageReporter : TextWriter
+{
+    public string lastOutput { get; private set; }
+
+    public override void Write(string value)
+    {
+        lastOutput = value;
+    }
+
+    public override void WriteLine(string value)
+    {
+        lastOutput = value;
+    }
+
+    public void Reset()
+    {
+        lastOutput = "";
+    }
+
+    public override Encoding Encoding
+    {
+        get { return Encoding.Default; }
+    }
+}
+
+static public class Mono
+{
+	static global::Mono.CSharp.Evaluator evaluator;
+	static MonoMessageReporter reporter = new MonoMessageReporter();
+	static public string lastOutput
+	{
+		get
+		{
+			var output = reporter.lastOutput;
+			reporter.Reset();
+			return output;
+		}
+	}
+
+	static public void Initialize()
+	{
+		var settings = new global::Mono.CSharp.CompilerSettings();
+		foreach (var assembly in System.AppDomain.CurrentDomain.GetAssemblies()) {
+			if (assembly == null) continue;
+			settings.AssemblyReferences.Add(assembly.FullName);
+		}
+		var printer = new global::Mono.CSharp.ConsoleReportPrinter(reporter);
+		var context = new global::Mono.CSharp.CompilerContext(settings, printer);
+		evaluator = new global::Mono.CSharp.Evaluator(context);
+	}
+
+	static public string Evaluate(string input, out object result, out bool result_set)
+	{
+		return evaluator.Evaluate(input, out result, out result_set);
+	}
+
+	static public bool Run(string input)
+	{
+		return evaluator.Run(input);
+	}
+
+	static public string[] GetCompletions(string input, out string prefix)
+	{
+		return evaluator.GetCompletions(input, out prefix);
+	}
+
+	static public string GetVars()
+	{
+		return evaluator.GetVars();
+	}
+
+	static public string GetUsing()
+	{
+		return evaluator.GetUsing();
+	}
+}
+
+}
